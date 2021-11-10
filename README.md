6a9b93bd25717bcf927602254f3914bfcd5a3a83
mthomas-ketchbrook <mthomas@ketchbrookanalytics.com>
2021-11-10 15:32:43 -0500

    Developed base app UI skeleton & theme

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
 
+<!-- README.md is generated from README.Rmd. Please edit that file -->
 
 # Calibration Training App <img src='www/university_logo.jpg' align="right" height="200" />
 
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of the Calibration Training App is to help the [University of
+Bonn](https://www.uni-bonn.de/en/university/) administer *calibration*
+assessments …
+
+## Installation
+
+1.  Clone this repository to your local machine
+
+2.  Open the **calibration\_app.R** file from the directory on your
+    local machine where you cloned this repository. This should install
+    the {renv} package if you do not already have it installed, but if
+    you don’t see that happen in the console, run
+    `install.packages("renv")`.
+
+3.  Run `renv::restore()` to install the package dependencies needed to
+    run this app successfully
+
+4.  Open the app.R file and execute the code in that script to launch
+    the app
+
+------------------------------------------------------------------------
+
+See below for session info for full reproducibility:
+
+``` r
+sessionInfo()
+#> R version 4.1.1 (2021-08-10)
+#> Platform: x86_64-w64-mingw32/x64 (64-bit)
+#> Running under: Windows 10 x64 (build 19042)
+#> 
+#> Matrix products: default
+#> 
+#> locale:
+#> [1] LC_COLLATE=English_United States.1252 
+#> [2] LC_CTYPE=English_United States.1252   
+#> [3] LC_MONETARY=English_United States.1252
+#> [4] LC_NUMERIC=C                          
+#> [5] LC_TIME=English_United States.1252    
+#> 
+#> attached base packages:
+#> [1] stats     graphics  grDevices datasets  utils     methods   base     
+#> 
+#> loaded via a namespace (and not attached):
+#>  [1] compiler_4.1.1  magrittr_2.0.1  fastmap_1.1.0   htmltools_0.5.2
+#>  [5] tools_4.1.1     yaml_2.2.1      stringi_1.7.5   rmarkdown_2.11 
+#>  [9] knitr_1.36      stringr_1.4.0   xfun_0.27       digest_0.6.28  
+#> [13] rlang_0.4.12    renv_0.14.0     evaluate_0.14
+```
--- app.R
+++ app.R
@@ -1,34 +1,98 @@
 
 library(shiny)
+library(bslib)
 
+# Load data outside of server, so it can be shared across all user sessions
+questions <- read.csv("data/example_questions_db.csv")
+
+app_theme <- bslib::bs_theme(
+  version = 5, 
+  bootswatch = "sketchy", 
+  primary = "#004F9E",   # Bonn blue
+  secondary = "#FBBA00"   # Bonn yellow
+)
 
 ui <- shiny::navbarPage(
   
   title = "Calibrator", 
   
+  theme = app_theme, 
+  
+  collapsible = TRUE, 
+  
   shiny::tabPanel(
     title = "Questions", 
     
-    shiny::verbatimTextOutput(
-      outputId = "tmp"
+    shiny::fluidRow(
+      
+      # Column for the assessment questions
+      shiny::column(
+        width = 7, 
+        shiny::wellPanel(
+          
+          shiny::h2("Question 1:"), 
+          
+          shiny::hr(), 
+          
+          shiny::p("The question ..."), 
+          
+          shiny::br(), 
+          
+          shiny::p("Placeholder for response options ..."), 
+          
+          shiny::hr(), 
+          
+          # 1.3 Previous / Next Buttons ----
+          # Create a button to go back to the prior question 
+          shiny::div(
+            
+            shiny::actionButton(
+              class = "btn btn-secondary", 
+              inputId = "back_btn",
+              label = "Previous",
+              icon = shiny::icon(name = "arrow-left")
+            ),
+            
+            # Create a button to go to the next question
+            shiny::div(
+              style = "float:right;", 
+              shiny::actionButton(
+                class = "btn btn-primary", 
+                inputId = "next_btn", 
+                label = "Next", 
+                icon = shiny::icon(name = "arrow-right")
+              )
+            )
+            
+          )
+          
+        )
+        
+      ), 
+      
+      # Column for results 
+      shiny::column(
+        width = 5, 
+        
+        shiny::h4("We'll make a reactive table containing the assessment results here")
+        
+      )
+      
     )
     
   )
   
 )
-
-
-
-
-server <- function(input, output, session) {
   
-  questions <- read.csv("data/example_questions_db.csv")
-  # TODO // assess whether `bindCache()` would be useful or not
   
-  output$tmp <- shiny::renderPrint(
-    questions$Question
-  )
   
-}
-
-shinyApp(ui, server)
+  
+  server <- function(input, output, session) {
+    
+    # output$tmp <- shiny::renderPrint(
+    #   questions$Question
+    # )
+    
+  }
+  
+  shinyApp(ui, server)
