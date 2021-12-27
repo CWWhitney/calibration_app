c72448451ac024cf818ef8d38339239c81e3f6fd
mthomas-ketchbrook <mthomas@ketchbrookanalytics.com>
2021-12-27 15:52:09 -0500

    Making column naming conventions consistent across pin

--- R/write_to_pin.R
+++ R/write_to_pin.R
@@ -5,7 +5,7 @@ write_to_pin <- function(board, type, data, user_first, user_last) {
   
   pins::pin_write(
     board = board, 
-    x = data %>% dplyr::mutate(user = glue::glue("{user_last}, {user_first}")), 
+    x = data %>% dplyr::mutate(User = glue::glue("{user_last}, {user_first}")), 
     name = glue::glue("{type}_{user_last}_{user_first}"), 
     type = "rds"
   )
