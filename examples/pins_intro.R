495830e3e50ed030ca24195802c902242fc2e3a2
CWWhitney <whitney.cory@gmail.com>
2022-04-13 11:14:05 +0200

    add notes to pons intro

--- examples/pins_intro.R
+++ examples/pins_intro.R
@@ -1,32 +1,37 @@
-
-
+# This is an overview for working with pins in the rsconnect system
 
 library(pins)
 
+# Connect to a board (ours is on rsconnect)
+# try other auth options (auth = "envvar") to get this running in rsconnect
+board <- pins::board_rsconnect(auth = "rsconnect") 
+
 # Connect to local board (this will *create* a local board if you don't already
 # have one)
-board <- pins::board_local()
+# board <- pins::board_local() 
 
-# Pin a dataset to the local board 
-pins::pin_write(
-  board = board, 
-  x = iris[1:30, ], 
-  name = "iris_test_1", 
-  type = "rds"
-)
+# # Pin a dataset to the local board 
+# pins::pin_write(
+#   board = board, 
+#   x = iris[1:30, ], 
+#   name = "iris_test_1", 
+#   type = "rds"
+# )
 
-# Pin a dataset to the local board 
-pins::pin_write(
-  board = board, 
-  x = iris[31:40, ], 
-  name = "iris_test_2", 
-  type = "rds"
-)
+# # Pin a dataset to the local board 
+# pins::pin_write(
+#   board = board, 
+#   x = iris[31:40, ], 
+#   name = "iris_test_2", 
+#   type = "rds"
+# )
 
 # List the pins with names that match "iris_test" 
 my_pins <- pins::pin_search(
   board = board, 
-  search = "iris_test"
+  search = "range_" #for example get all the data where people did the range questions
+  #change this search argument according to the part of the 
+  #workshop to be assessed _range 
 )
 
 my_pins
@@ -40,9 +45,11 @@ df <- my_pins %>%
   ) %>% 
   tibble::as_tibble()
 
-df
+df #downlod this as a .csv or other file format 
+# this can be used for further analysis
 
 # Remove the pinned datasets from the board
+# run this code each time you want to clear the board that was named 
 pins::pin_delete(
   board = board, 
   names = my_pins$name
