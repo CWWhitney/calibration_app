09eebeeb6c817bc57bff4b958bbd346e58a0cf2c
ChristineSchmitz <s7chschm@uni-bonn.de>
2023-11-21 09:31:49 +0100

    deactivate unused questions

--- global.R
+++ global.R
@@ -24,22 +24,22 @@ google_sheets_url <- "https://docs.google.com/spreadsheets/d/1yTboPXmDMF43YmjsuE
 selected_questions <- list(
   Group_1 = list(
     binary = c(3, 7, 20, 23, 24, 102, 103, 104, 105, 106)
-  ), 
-  Group_2 = list(
-    binary = c(25:30)
-  ), 
+  )#, 
+  # Group_2 = list(
+  #    binary = c(25:30)
+  # ), 
   
-  Group_3 = list(
-    binary = c(31:40)
-  ), 
+  #Group_3 = list(
+  # binary = c(31:40)
+  #), 
   
-  Group_4 = list(
-    binary = c(41:50)
-  ), 
+  #Group_4 = list(
+  #  binary = c(41:50)
+  #), 
   
-  Group_5 = list(
-    binary = c(164:173)
-  )
+  #  Group_5 = list(
+  #    binary = c(164:173)
+  #  )
 )
 
 # Connect to the {pins} board for this workshop
