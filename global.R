05e2422f0bd95a955aebd8723a5a894323b820ff
ChristineSchmitz <s7chschm@uni-bonn.de>
2023-11-21 09:30:57 +0100

    deactivate unused questions

--- global.R
+++ global.R
@@ -28,22 +28,22 @@ selected_language<-pull(interface_languages[,language],language)
 selected_questions <- list(
   Group_1 = list(
     binary = c(3, 7, 20, 23, 24, 102, 103, 104, 105, 106)
-  ), 
-  Group_2 = list(
-    binary = c(25:30)
-  ), 
+  )#, 
+ # Group_2 = list(
+#    binary = c(25:30)
+ # ), 
   
-  Group_3 = list(
-    binary = c(31:40)
-  ), 
+  #Group_3 = list(
+   # binary = c(31:40)
+  #), 
   
-  Group_4 = list(
-    binary = c(41:50)
-  ), 
+  #Group_4 = list(
+  #  binary = c(41:50)
+  #), 
   
-  Group_5 = list(
-    binary = c(164:173)
-  )
+#  Group_5 = list(
+#    binary = c(164:173)
+#  )
 )
 
 
