43604e042a40652422975bae43a314c2ac3fb558
ChristineSchmitz <s7chschm@uni-bonn.de>
2024-03-20 14:12:29 +0100

    Back to German

--- global.R
+++ global.R
@@ -9,7 +9,7 @@ library(purrr)
 # User-Defined Parameters -------------------------------------------------
 
 # Define the language questions will be asked in
-language <- "English"
+language <- "German"
 
 # Define Google API authentication type
 # If the Google Sheet is public, simply call `googlesheets4::gs4_death()` here
@@ -27,29 +27,34 @@ selected_language<-pull(interface_languages[,language],language)
 #### Change and use these to set up each workshop
 selected_questions <- list(
   Group_1 = list(
-    range = c(1, 3, 7, 11, 12, 13, 36, 39, 40, 41)
-  ),
-  Group_2 = list(
-    range = c(43, 45, 46, 47, 48, 49, 50, 51, 52, 53)
-  ),
-  Group_3 = list(
-    range = c(314, 63, 65,  69, 71, 72, 86, 91, 93, 100)
-  ),
-  Group_4 = list(
-    range = c(106, 110, 114, 115, 116, 117, 118, 119, 121, 122)
-  ),
+    range = c(1, 2, 4, 7, 8, 11, 14, 17, 19, 30) 
+  ), 
+  
+  Group_2 = list( 
+    range = c(39, 46, 50, 64, 67, 85, 93, 97, 100, 285)
+  ), 
+  
+  Group_3 = list( 
+    range = c(104, 136, 138, 140, 144, 152, 154, 156, 160, 162)
+  ), 
+  
+  Group_4 = list( 
+    range = c( 170, 173, 174, 183, 208, 213, 214, 219, 221, 225)
+  ), 
+  
   Group_5 = list(
-    range = c(124, 126, 127, 128, 129, 130, 131, 132, 133, 135)
-  ),
-  Group_6 = list(
-    range = c(158, 159, 60, 253, 294, 300, 301, 302, 308, 309, 312)
-  ),
-  Group_7 = list(
-    range = c(310, 311, 313, 62, 315, 316, 317, 318, 307, 292)
-  ),
-  Group_8 = list(
-    range = c(284, 241, 227, 164, 152, 120, 99 ,96, 92, 67)
-  )
+    range = c(242, 246, 250, 253, 259, 261, 262, 271, 274, 283)
+  )#,
+  #Group_6 = list(
+  #  range = c(112, 114, 115, 116, 117, 118, 119, 120, 121, 122)
+  #),
+  #Group_7 = list(
+  #  range = c(123, 124, 125, 126, 127, 128, 129, 130, 131, 132)
+  #),
+  #Group_8 = list(
+  #  range = c(133, 135, 152, 158, 159, 166, 219, 227,291, 292, 294, 295, 300, 301, 302)
+
+  #)
 )
 # Connect to the {pins} board for this workshop
 # this is on Rstudio Connect 
