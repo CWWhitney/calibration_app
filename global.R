012c7dbfefab41817147bf0f3a966ee5a3d3fd86
ChristineSchmitz <s7chschm@uni-bonn.de>
2024-11-18 13:13:41 +0100

    neuer test

--- global.R
+++ global.R
@@ -52,7 +52,10 @@ selected_questions <- list(
 # this is on Rstudio Connect 
 #board <- pins::board_temp() # auth = "auto", "manual", "envvar", "rsconnect"
 #board<-pins::board_rsconnect()
-board<-pins::board_connect()
+board<-pins::board_connect(
+  auth="rsconnect",
+  key = api_calibration
+)
 options(scipen=1000000000)
 
 # DO NOT EDIT CODE BELOW THIS LINE ----------------------------------------
