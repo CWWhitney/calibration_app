abcabdca8f052d5e9371e2c836ecbcca8aef6cda
ChristineSchmitz <s7chschm@uni-bonn.de>
2024-11-18 10:41:31 +0100

    test2

--- global.R
+++ global.R
@@ -51,8 +51,8 @@ selected_questions <- list(
 # Connect to the {pins} board for this workshop
 # this is on Rstudio Connect 
 #board <- pins::board_temp() # auth = "auto", "manual", "envvar", "rsconnect"
-#board<-pins::board_rsconnect()
-board<-pins::board_connect(auth = "rsconnect")
+board<-pins::board_rsconnect()
+#board<-pins::board_connect(auth = "rsconnect")
 options(scipen=1000000000)
 
 # DO NOT EDIT CODE BELOW THIS LINE ----------------------------------------
