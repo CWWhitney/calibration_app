091591b21037e949f765c5b2d93a822abcb7fa09
ChristineSchmitz <s7chschm@uni-bonn.de>
2025-06-11 11:14:35 +0200

    same qestions as in the German version

--- global.R
+++ global.R
@@ -27,24 +27,25 @@ selected_language<-pull(interface_languages[,language],language)
 #### Change and use these to set up each workshop
 selected_questions <- list(
   Group_1 = list(
-    range =  c(88,338,100,325,344,12,330,347,322,293)
+    range =  c(5,12,14,16,20,35,53,100,101,102)
   ), 
   
   Group_2 = list( 
-    range =  c(329,341,119,69,334,346,46,333,327,339)
+    range =  c(2,3,103,146,147,150,156,157,158,207)
   ), 
   
   Group_3 = list( 
-    range = c(169,342,336,34,345,326,340,337,158,164)
+    range = c(104,105,155,162,209,211,242,257,260,262)
   ), 
   
   Group_4 = list( 
-    range = c(64,160,159,261,253,293,158,169,166,117)
-  )#, 
+    range = c(247:256)
+  ), 
   
-  #Group_5 = list(
-  #  range = c(268,223,227,215,213,175,150,90,10,256)
-  #)#,
+  Group_5 = list(
+    range = c(268,223,227,215,213,175,150,90,10,258)
+  )#,
+  
   #Group_6 = list(
   #  range = c(112, 114, 115, 116, 117, 118, 119, 120, 121, 122)
   #),
